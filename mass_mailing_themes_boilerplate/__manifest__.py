aaccd55c39d692509079d25c69df81cba502444f
Christian Kanja <christian.kanja@glueckkanja-gab.com>
2021-04-05 15:40:16 +0200

    mass-mailing v1540

---
+++ mass_mailing_themes_boilerplate/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Mass Mailing Themes Community Boilerplate",
+    "summary": "Mass Mailing Boilerplate Theme",
+    "description": """
+A boilerplate for the community to understand how to build their own custom Odoo Mass Mailing Themes
+    """,
+    "version": "10.0.1.0.0",
+    "category": "Marketing",
+    "author": 'Luke Branch, Odoo Community Widgets',
+    "depends": [
+        "mass_mailing",
+    ],
+    "data": [
+	"views/email_designer_snippets.xml",
+	"views/theme_boilerplate_template.xml",
+    ],
+    "qweb": [],
+    "demo": [
+    ],
+    "website": "https://github.com/OdooCommunityWidgets/mass_mailing_themes_community/new/10.0",
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+}
